f0884739ca6fc770c525474b05d4352d6ce58d22
demonassit <jminorg@ipn.mx>
2022-03-11 17:52:24 -0600

    asi no se suvbe

--- 06JavaScript/js/interes.js
+++ 06JavaScript/js/interes.js
@@ -10,3 +10,19 @@ function validarn(e){
     var codigo = String.fromCharCode(teclado);
     return patron.test(codigo);
 }
+
+function interes(){
+
+    var valor = document.formulario.cantidad.value;
+    var resul = parseInt(valor);
+    var interes = resul*0.037;
+    var total = interes + resul;
+
+    document.formulario.sueldoI.value= "$" + total;
+}
+
+function borrard(){
+
+    document.formulario.cantidad.value="";
+    document.formulario.sueldoI.value="";
+}
